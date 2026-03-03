a8a9fe98bf82733fd33450be21806ab2f3b4e989
Ayush <ayushbahuguna006@gmail.com>
2026-03-04 00:26:44 +0530

    React-router dom added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,12 +2,27 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './Pages/Home';
+import Blogs from './Pages/Blogs';
+import About from './Pages/About';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
+import Navbar from './Components/Navbar';
 
 function App() {
- 
+
   return (
     <>
-     <h1 className='text-red-500 text-center'>hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<><Navbar/><Home /></>} />
+          <Route path="/blogs" element={<><Navbar/><Blogs /></>} />
+          <Route path="/about" element={<><Navbar/><About /></>} />
+          <Route path="/login" element={<><Navbar/><Login /></>} />
+          <Route path="/signup" element={<><Navbar/><Signup /></>} />
+        </Routes>
+      </BrowserRouter>,
     </>
   )
 }
