e3b465993b610a22a3c656ba2613f31900e8ca03
Ayush <ayushbahuguna006@gmail.com>
2026-03-03 09:28:15 +0530

    project reinitiated

--- server.js/models/user.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose')
-const userschema=mongoose.Schema({
-    name:{type:String,required:true,unique:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true,unique:true},
-})
-const User=mongoose.model('user',userschema)
-module.exports=User;
--- server.js/routes/user.js
+++
@@ -1,6 +0,0 @@
-const express=require("express")
-const router=express.Router()
-const {handleusersignup,handleuserlogin}=require('../controllers/user')
-router.post('/signup',handleusersignup)
-router.post('/login',handleuserlogin)
-module.exports=router
